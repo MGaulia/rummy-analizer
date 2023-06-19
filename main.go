7783a638a6a7a527c4649fa87999972f6b5e2a43
Matas Gaulia <39590298+MGaulia@users.noreply.github.com>
2023-06-19 22:36:29 +0200

    program run code

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"main/mapper"
+	"main/model"
+	"os"
+)
+
 func main() {
+	var table []model.Card
+	var input string
+	for input != "exit" {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter text: ")
+		input, _ = reader.ReadString('\n')
+
+		card, err := mapper.StringToCard(input)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			table = append(table, card)
+			fmt.Println(table)
+		}
+	}
 }
